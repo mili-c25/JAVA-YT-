99071bfd6d8ff2b5d8e5c8298eda8c13f567b19e
mili-c25 <soumilic61@gmail.com>
2025-06-24 21:19:30 +0530

    Add files via upload

---
+++ greeting.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class greeting {
+    public static void main(String[] args) {
+        greeting();
+        
+    }
+    static void greeting() {
+        System.out.println("Always keep smiling,even when life gets tough.");
+    }
+    
+}
