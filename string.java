e5643ccd8f0b2b400963c3d333cdd0151168d398
mili-c25 <soumilic61@gmail.com>
2025-07-06 00:43:19 +0530

    Add files via upload

--- string.java
+++ string.java
@@ -1,20 +1,17 @@
 import java.util.*;
-
 public class string {
     public static void main(String[] args) {
-    // String message = greet();
-    //System.out.println(message);
-    Scanner in = new Scanner(System.in);
-    System.out.print("Enter a name:");
-    String name = in.next();
-    String personalised = myGreet(name);
-    System.out.println(personalised);
+        Scanner sc = new Scanner(System.in);
+        String[] str = new String[4];
+        for (int i = 0; i < str.length; i++) {
+            str[i] = sc.next();
+        }
+
+        System.out.println(Arrays.toString(str));
+
+        // modify
+        str[1] = "kunal";
+
+        System.out.println(Arrays.toString(str));
     }
-static String myGreet(String name) {
-     String message = "Hello "   +   name;
-     return message;
 }
-
-    
-    }
-
